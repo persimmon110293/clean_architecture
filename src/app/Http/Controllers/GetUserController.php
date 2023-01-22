9dca9f97a3b63e15df29fec81209d6e6af54e4e8
Shohei Kakimoto <shohei.11293@gmail.com>
2023-01-22 22:54:19 +0900

    add: add response files

--- src/app/Http/Controllers/GetUserController.php
+++ src/app/Http/Controllers/GetUserController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\GetUserRequest;
 use App\Packages\UseCases\User\Get\IGetUserUseCase;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
 
 class GetUserController extends Controller
 {
-    public function __invoke(GetUserRequest $request, IGetUserUseCase $interactor)
+    public function __invoke(GetUserRequest $request, IGetUserUseCase $interactor, UserResource $resource): JsonResponse
     {
         $user = $interactor->getUser($request->all());
-        dd($user);
-        // return $interactor->getUser($request->all());
+        return response()->json($resource->toArray($user));
     }
 }
